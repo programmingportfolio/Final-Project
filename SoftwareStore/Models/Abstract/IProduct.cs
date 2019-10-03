d584d0aeced468519919a87eba9c89ed211bc927
LUke <porterl2268@student.faytechcc.edu>
2019-10-02 20:57:44 -0400

    creating seed data

--- SoftwareStore/Models/Abstract/IProduct.cs
+++ SoftwareStore/Models/Abstract/IProduct.cs
@@ -7,30 +7,32 @@ namespace SoftwareStore.Models.Abstract
 {
     interface IProduct
     {
-         int managerId { get; set; }
+        string UserName { get; set; }
 
-         string productName { get; set; }
-         string productCategory { get; set; }
-         string shortDescription { get; set; }
-         string longDescription { get; set; }
-         decimal price { get; set; }
-         int quantity { get; set; }
-         string easyUrl { get; set; }
-         string videoUrl { get; set; }
-         string pictureUrl1 { get; set; }
-         string pictureUrl2 { get; set; }
-         string pictureUrl3 { get; set; }
-         string pictureUrl4 { get; set; }
-         string pictureUrl5 { get; set; }
-         string pictureUrl6 { get; set; }
-         string pictureUrl7 { get; set; }
-         string pictureUrl8 { get; set; }
-
-        //public List<string> pictureUrls = new List<string>();
-        //public static int pictureCount { get; set; }
-        //public bool hasPictures { get; set; }
+        string ProductName { get; set; }
+        string ProductCategory { get; set; }
+        string ShortDescription { get; set; }
+        string LongDescription { get; set; }
+        decimal Price { get; set; }
+        int Quantity { get; set; }
+        string EasyUrl { get; set; }
+        string VideoUrl { get; set; }
+        string PictureUrl1 { get; set; }
+        string PictureUrl2 { get; set; }
+        string PictureUrl3 { get; set; }
+        string PictureUrl4 { get; set; }
+        string PictureUrl5 { get; set; }
+        string PictureUrl6 { get; set; }
+        string PictureUrl7 { get; set; }
+        string PictureUrl8 { get; set; }
+    }
+}
+        /*
+        // List<string> pictureUrls = new List<string>();
+        // static int pictureCount { get; set; }
+        // bool hasPictures { get; set; }
         bool hasVideo { get; set; }
-        //public int productCount { get; set; }
+        // int productCount { get; set; }
 
 
 
@@ -41,6 +43,4 @@ namespace SoftwareStore.Models.Abstract
         void OnChange();
 
         string ToString();
-
-    }
-}
+        */
