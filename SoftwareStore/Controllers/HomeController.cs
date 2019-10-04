f2c6e7f7fbe50135d07c9cadb60f84023fdeb603
LUke <porterl2268@student.faytechcc.edu>
2019-10-03 23:09:28 -0400

    transferred html to views

--- SoftwareStore/Controllers/HomeController.cs
+++ SoftwareStore/Controllers/HomeController.cs
@@ -20,6 +20,59 @@ namespace SoftwareStore.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Checkout1()
+        {
+            return View();
+        }
+        public IActionResult Checkout2()
+        {
+            return View();
+        }
+        public IActionResult Checkout3()
+        {
+            return View();
+        }
+        public IActionResult Checkout4()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
